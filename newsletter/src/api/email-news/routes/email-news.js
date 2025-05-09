e8c81200e59579ac5678f2597335857d8d093785
Mark Munyaka <markmunyaka@gmail.com>
2025-05-09 12:18:46 +0000

    Send email to collection (All users): Add New API Route

--- newsletter/src/api/email-news/routes/email-news.js
+++ newsletter/src/api/email-news/routes/email-news.js
@@ -8,5 +8,11 @@ module.exports = {
         auth: false, // Set to true if authentication is required
       },
     },
+    {
+      method: "POST",
+      path: "/send-email-to-all",
+      handler: "email-news.sendToAllSubscribers",
+      config: { auth: false },
+    }
   ],
 };
